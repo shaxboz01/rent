6dbcddd6570ba90bbafb21478e8b03e26a59b4e5
shaxboz01 <shaxbozaxrorov01@gmail.com>
2024-02-09 19:14:03 +0500

    final one

--- conf/settings.py
+++ conf/settings.py
@@ -164,5 +164,5 @@ MEDIA_ROOT = os.path.join( BASE_DIR,   "static /")
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'main.MyUserManager'
+
 
